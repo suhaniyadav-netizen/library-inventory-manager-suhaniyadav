aae9dfede06a7083bb1d72e58c23ca749743c0ad
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2025-11-24 11:42:50 +0530

    Add files via upload

---
+++ cli/book.py
@@ -0,0 +1,35 @@
+# LIBRARY - INVENTORY - MANAGER by < SUHANI YADAV >
+# Date : 19th November 2025
+
+class Book:
+    def __init__(self, title, author, isbn, status='available'):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.status = status
+
+    def issue(self):
+        if self.status == 'available':
+            self.status = 'issued'
+            return True
+        return False
+
+    def return_book(self):
+        if self.status == 'issued':
+            self.status = 'available'
+            return True
+        return False
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (ISBN: {self.isbn}, Status: {self.status})"
+
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'isbn': self.isbn,
+            'status': self.status
+        }
+
+    def is_available(self):
+        return self.status == 'available'
